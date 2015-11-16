f386af2f6cc94405ea2769f6283d3a3f725a4b90
eugen <tassadars@gmail.com>
2015-11-16 10:05:39 +0200

    Display plain parameters from edit_area and edit_box

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,10 @@ class ArticlesController < ApplicationController
 
   def new
   end
-  
+
+  def create
+      # key of hash,      value of hash
+      render plain: params[:article].inspect
+  end
+
 end
