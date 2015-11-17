73b77e76fc60899644e3e0f86e131385f5387b36
eugen <tassadars@gmail.com>
2015-11-17 12:51:24 +0200

    Save Contact to DB in safe way

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,16 @@ class ContactsController < ApplicationController
   end
 
   def create
+    # we change params[:contact] to contact_params. Receive params from browser in safe way
+    @contact = Contact.new(contact_params)    
+    @contact.save
+  end
+
+  private
+
+  # safe way to permit params from browser
+  def contact_params
+    params.require(:contact).permit(:email, :message)  
   end
 
 end
