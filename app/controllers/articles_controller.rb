8edac785b91f0806070948c9d3a196dd87c4db68
eugen <tassadars@gmail.com>
2015-11-18 17:33:36 +0200

    Implement articles#edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,25 +4,37 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def new
-  end
-
   def show
     @article = Article.find(params[:id])
+  end
 
+  def new
   end
 
   def create
     # we change params[:contact] to contact_params. Receive params from browser in safe way
     @article = Article.new(article_params)    
-    if @article.valid?
-      @article.save
+    if @article.save #autocheck validation, no need to .valid?
       redirect_to @article # it automatically decide where to redirect, now it's #show
     else
       render action: 'new'
     end
   end
 
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params) #autocheck validation, no need to .valid?
+      redirect_to @article # it automatically decide where to redirect, now it's #update
+    else
+      render action: 'edit'
+    end
+  end
+  
   private
 
   # safe way to permit params from browser. hacker will not be able to 
