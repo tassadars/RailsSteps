841e9b0cc398d30182f3632a5e516006bbdf2a28
eugen <tassadars@gmail.com>
2015-11-18 13:32:06 +0200

    Implement articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,22 @@ class ArticlesController < ApplicationController
   end
 
   def create
-      # key of hash,      value of hash
-      render plain: params[:article].inspect
+    # we change params[:contact] to contact_params. Receive params from browser in safe way
+    @article = Article.new(article_params)    
+    if @article.valid?
+      @article.save
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  # safe way to permit params from browser. hacker will not be able to 
+  # put as param another field, like user password or else if it's not 
+  # article, but user entity for example
+  def article_params
+    params.require(:article).permit(:title, :text)  
   end
 
 end
