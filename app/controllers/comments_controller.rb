f25fc792ff06fe9ab082e2d1ee33501771087eb6
eugen <tassadars@gmail.com>
2015-11-19 17:06:31 +0200

    Add comments to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+
+    # we don't assign, because in this cace, no need.
+    # @comment = @article.comments.create(comment_params)
+    @article.comments.create(comment_params)
+
+    redirect_to article_path(@article)
+  end
+
+
+  private
+
+  # safe way to permit params from browser
+  def comment_params
+    params.require(:comment).permit(:author, :body)  
+  end
+
 end
