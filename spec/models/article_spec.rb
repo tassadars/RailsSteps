6d1daf27094462e2000d2103862611cbc3b78022
eugen <tassadars@gmail.com>
2015-11-24 17:46:17 +0200

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,12 +2,22 @@ require "spec_helper"
 
 describe Article do 
 
-describe "validations" do
-  it { should validate_presence_of :title }      
-  it { should validate_presence_of :text }    
-end
+  describe "validations" do
+    it { should validate_presence_of :title }      
+    it { should validate_presence_of :text }    
+  end
 
-describe "associations" do
-  it { should have_many :comments }   
-end
+  describe "associations" do
+    it { should have_many :comments }   
+  end 
+
+  describe "#subject" do
+    it "returns the article title" do
+      # создаем объект article хитрым способом
+      article = create(:article, title: 'Lorem Ipsum')
+
+      #assert, проверка
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
 end
