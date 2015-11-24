ec4c6ff58fdf9c3a6accca1325ab6b39fe0ce251
eugen <tassadars@gmail.com>
2015-11-24 11:25:00 +0200

    Move edit, del btns to helper zone

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,10 @@
 module ArticlesHelper
+
+
+  def show_edit_del_btns article
+    result = link_to ' | Edit', edit_article_path(article) 
+    result += link_to ' | Delete', article, data: { :confirm => 'Are you sure?'}, :method => :delete
+    result.html_safe
+  end
+
 end
