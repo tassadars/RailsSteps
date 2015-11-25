a3518d25b40610bfd84d35e4876f5e660e2d10be
eugen <tassadars@gmail.com>
2015-11-25 18:01:22 +0200

    Add test with multilanguage support

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -4,6 +4,6 @@ require "spec_helper"
 feature "Contact creation" do 
   scenario "allows access to contact page" do
     visit '/contacts'
-    expect(page).to have_content 'Contact us'
+    expect(page).to have_content  I18n.t('contacts.contact_us')
   end
 end
