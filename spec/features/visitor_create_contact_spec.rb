0154ebe3a32a7e39afd932e777d5a02cc1bc3f20
eugen <tassadars@gmail.com>
2015-11-25 18:17:49 +0200

    Update test, allow guest to create contact message

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -6,4 +6,16 @@ feature "Contact creation" do
     visit '/contacts'
     expect(page).to have_content  I18n.t('contacts.contact_us')
   end
+
+  scenario "allows a guest to create contact" do
+    visit '/contacts'
+
+    # will find by input edit id="contact_email". Use inspect element on page to see this name
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'Something' 
+
+    click_button 'Send message'
+
+    expect(page).to have_content 'Thanks'
+  end
 end
