4509f3bdfd0e05e42883010d4897c19bcec277c4
eugen <tassadars@gmail.com>
2015-11-26 18:08:31 +0200

    Test of article edit success

--- spec/features/user_update_article_spec.rb
+++ spec/features/user_update_article_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 feature "Article update" do 
   before(:each) do 
     sign_up
-    @article = create(:article, title: 'Some bug if not to initialize this field', text: 'Lorem Ipsum hum hum')
+    @article = create(:article, title: 'mike1', text: 'Lorem Ipsum hum hum')
   end
 
   scenario "allows user to visit edit article page" do
@@ -15,6 +15,20 @@ feature "Article update" do
     expect(page).to have_content 'Lorem Ipsum hum hum'
     #visit (article_path(@article) + '/edit')
   end
+
+
+  scenario "allows user to update article" do
+    visit article_path(@article) + '/edit'
+
+    new_msg = 'New test message!!!'
+    # will find by input edit id="contact_email". Use inspect element on page to see this name
+    fill_in :article_text, :with => new_msg
+
+    click_button 'Save Article' #I18n.t('articles.save_article')
+
+    expect(page).to have_content new_msg
+  end
+
 
 
 end
