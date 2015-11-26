a5b57eaa58421706031bf14ea76aef44c40991bf
eugen <tassadars@gmail.com>
2015-11-26 14:21:11 +0200

    update article test. add multilang supportt

--- spec/features/user_create_article_spec.rb
+++ spec/features/user_create_article_spec.rb
@@ -8,7 +8,7 @@ feature "Article creation" do
 
   scenario "allows user to visit new article page" do
     visit new_article_path
-    expect(page).to have_content 'New article'
+    expect(page).to have_content I18n.t('articles.new_article')
   end
 end
 
